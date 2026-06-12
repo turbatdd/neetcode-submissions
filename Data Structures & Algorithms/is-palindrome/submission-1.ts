10d726fefeef549011328f0b7e1ac7f8e0e1816d
turbatdd <49607815+turbatdd@users.noreply.github.com>
2026-06-12 11:47:47 -0500

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        s = s.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+        let l: number = 0;
+        let r: number = s.length - 1;
+
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
