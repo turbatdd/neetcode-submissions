837a73324fe52832460405981dc4fe704c5118bd
turbatdd <49607815+turbatdd@users.noreply.github.com>
2026-06-13 20:56:19 -0500

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    removeDuplicates(nums: number[]): number {
+        let count = 1;
+
+        for (let i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[count++] = nums[i];
+            }
+        }
+
+        return count;
+    }
+}
