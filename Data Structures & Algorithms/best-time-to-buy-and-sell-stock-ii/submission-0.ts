b49e40ec4ee955ff62db2a917aa0581fd04d64cd
turbatdd <49607815+turbatdd@users.noreply.github.com>
2026-06-24 09:08:36 -0500

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let result:number = 0;
+
+        for(let i=1; i<prices.length; i++){
+            if(prices[i]>prices[i-1]){
+                result+= prices[i] - prices[i-1];
+            }
+
+        }
+
+        return result;
+    }
+}
